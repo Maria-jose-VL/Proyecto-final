fef6d883119b0da6c986e619f1a41146f8457f06
JuanPabloJurado <167464417+Jpexynos@users.noreply.github.com>
2024-05-11 16:52:35 -0500

    Update Administrador.java

--- src/main/java/co/edu/uniquindio/poo/Administrador.java
+++ src/main/java/co/edu/uniquindio/poo/Administrador.java
@@ -1,9 +1,40 @@
 package co.edu.uniquindio.poo;
 
-    public class Administrador extends Persona{
+public class Administrador extends Persona {
+    private double tarifaMotoClasica;
+    private double tarifaMotoHibrida;
+    private double tarifaCarro;
 
-        public Administrador(String nombre, String apellido, String telefono, String identificacion) {
-            super(nombre, apellido, telefono, identificacion);
-            //TODO Auto-generated constructor stub
-        }   
+    public Administrador(String nombre, String apellido, String telefono, String identificacion,
+                         double tarifaMotoClasica, double tarifaMotoHibrida, double tarifaCarro) {
+        super(nombre, apellido, telefono, identificacion);
+        this.tarifaMotoClasica = tarifaMotoClasica;
+        this.tarifaMotoHibrida = tarifaMotoHibrida;
+        this.tarifaCarro = tarifaCarro;
+    }
+
+    // Getters y setters para los atributos de tarifa
+    public double getTarifaMotoClasica() {
+        return tarifaMotoClasica;
+    }
+
+    public void setTarifaMotoClasica(double tarifaMotoClasica) {
+        this.tarifaMotoClasica = tarifaMotoClasica;
+    }
+
+    public double getTarifaMotoHibrida() {
+        return tarifaMotoHibrida;
+    }
+
+    public void setTarifaMotoHibrida(double tarifaMotoHibrida) {
+        this.tarifaMotoHibrida = tarifaMotoHibrida;
+    }
+
+    public double getTarifaCarro() {
+        return tarifaCarro;
+    }
+
+    public void setTarifaCarro(double tarifaCarro) {
+        this.tarifaCarro = tarifaCarro;
+    }
 }
